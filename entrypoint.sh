12a68b44b31e6cfa4a81f61ef082add01ea8e0e0
jachin <ygcaicn@gmail.com>
2021-01-21 23:57:45 +0800

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#Xray版本
+if [[ -z "${VER}" ]]; then
+  VER="latest"
+fi
+echo ${VER}
+
+if [[ -z "${Xray_Path}" ]]; then
+  Xray_Path="/s233"
+fi
+echo ${Xray_Path}
+
+if [ "$VER" = "latest" ]; then
+  VER=`wget -qO- "https://api.github.com/repos/XTLS/Xray-core/releases/latest" | sed -n -r -e 's/.*"tag_name".+?"([vV0-9\.]+?)".*/\1/p'`
+  [[ -z "${VER}" ]] && VER="v1.2.2"
+else
+  VER="v$VER"
+fi
+
+mkdir /xraybin
+cd /xraybin
+RAY_URL="https://github.com/XTLS/Xray-core/releases/download/${VER}/Xray-linux-64.zip"
+echo ${RAY_URL}
+wget --no-check-certificate ${RAY_URL}
+unzip Xray-linux-64.zip
+rm -f Xray-linux-64.zip
+chmod +x ./xray
+ls -al
+
+cd /wwwroot
+tar xvf wwwroot.tar.gz
+rm -rf wwwroot.tar.gz
+
+
+sed -e "/^#/d"\
+    -e "s|\${INBOUND}|${INBOUND}|g"\
+    /conf/Xray.template.json >  /xraybin/.config.json
+
+sed -e "/^#/d"\
+    -e "s|\${Xray_Path}|${Xray_Path}|g"\
+    /xraybin/.config.json >  /xraybin/config.json
+
+echo /xraybin/config.json
+cat /xraybin/config.json
+
+if [[ -z "${ProxySite}" ]]; then
+  s="s/proxy_pass/#proxy_pass/g"
+  echo "site:use local wwwroot html"
+else
+  s="s|\${ProxySite}|${ProxySite}|g"
+  echo "site: ${ProxySite}"
+fi
+
+sed -e "/^#/d"\
+    -e "s/\${PORT}/${PORT}/g"\
+    -e "s|\${Xray_Path}|${Xray_Path}|g"
+    -e "$s"\
+    /conf/nginx.template.conf > /etc/nginx/conf.d/ray.conf
+echo /etc/nginx/conf.d/ray.conf
+cat /etc/nginx/conf.d/ray.conf
+
+
+cd /xraybin
+./xray run -c ./config.json &
+rm -rf /etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
